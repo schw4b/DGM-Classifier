d960600ffe5fea9132cb5da2519a394f43681ed5
Simon Schwab <schw4b@gmail.com>
2018-03-19 10:42:58 +0000

    Update

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -6,7 +6,7 @@ file='ukb8252.tab'
 # Create list of EIDS
 cat $PATH_IMG/subjects/subj.txt $PATH_IMG/subjects2/subj.txt $PATH_IMG/subjects3/subj.txt > $PATH_BB/subjects-090318.txt
 
-# create decouple ID from BB for speed
+# Detach IDs from BB for speed
 cat $file | awk '{ print $1}' > $file.ids
 
 # Extract imaging UKBB individuals
@@ -19,3 +19,5 @@ cat header.tab lines/* > ukbiobank.tab
 
 # Clean up
 # Manually replace Builders Merchant\"s with Builders Merchant\'s
+
+# See UKBB-MH.R for further processing
