119747509e8063b4a6873531a6672f9ab1b4994a
Simon Schwab <schw4b@gmail.com>
2018-04-19 11:09:48 +0200

    update

--- submit/job-DMN-Slurm.sh
+++ submit/job-DMN-Slurm.sh
@@ -2,12 +2,12 @@
 
 #SBATCH --mail-type=FAIL
 #SBATCH --mail-user=schwab@upd.unibe.ch
-#SBATCH --time=00:30:00
-#SBATCH --job-name=DGM-Nn14
+#SBATCH --time=25:00:00
+#SBATCH --job-name=DGM-DMN
 #SBATCH --mem-per-cpu=512M
 #SBATCH --output=log/slurm/slurm-%j.out
 #SBATCH --error=log/slurm/slurm-%j.out
-#SBATCH --array=1-1890
+#SBATCH --array=1-2000
 
 # submit is a symlink
-srun submit/DGM-Nn14-Slurm.R
+srun submit/DGM-DMN-Slurm.R
