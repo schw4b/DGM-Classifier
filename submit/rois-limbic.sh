218cb5a03668412de920624655b844da3bb60484
Simon Schwab <schw4b@gmail.com>
2018-05-22 15:01:43 +0100

    Update

--- submit/rois-limbic.sh
+++ submit/rois-limbic.sh
@@ -17,16 +17,19 @@ INFO="limbic"
 PATH_FMRI="/vols/Data/ukbiobank/FMRIB/IMAGING/data3/SubjectsAll"
 PATH_PROJ="${HOME}/UKBB-MH"
 FILE_ATLAS="${PATH_PROJ}/atlas/limbic.nii.gz"
+FILE_BRAINMASK="${PATH_PROJ}/atlas/brain_mask.nii.gz"
 LOG="${PATH_PROJ}/log/roi-limbic.log"
 
 MYID=$(echo $(sed "${SGE_TASK_ID}q;d" ${PATH_PROJ}/results/subjectsN158.txt) | awk '{ print $1 }')
 
 FILE="${PATH_FMRI}/${MYID}/fMRI/rfMRI.ica/reg_standard/filtered_func_data_clean.nii.gz"
 OUT="${HOME}/Drive/${INFO}/${MYID}_${INFO}.txt"
+OUTGS="${HOME}/Drive/${INFO}/${MYID}_${INFO}-globalSignal.txt"
 
 if [[ -e "${FILE}" && ! -e "${OUT}" ]] ; then
     fslmeants -i ${FILE} --label=${FILE_ATLAS} -o ${OUT}
-    echo $(date) $(echo fslmeants -i ${FILE} -m ${PATH_ATLAS}/${ROI} -o ${OUT}) >> $LOG
+    echo $(date) $(echo fslmeants -i ${FILE} --label=${FILE_ATLAS} -o ${OUT}) >> $LOG
+    fslmeants -i ${FILE} -m ${FILE_BRAINMASK} -o ${OUTGS}
 elif [[ ! -e "${FILE}" ]] ; then
     echo $(date) ERROR ${FILE} not found >> $LOG
 fi
